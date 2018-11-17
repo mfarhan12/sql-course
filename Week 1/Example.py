8af5eb2c8bf213e09985146cccf3173a3f331aa9
Mohammad Farhan <mfarhan102@gmail.com>
2018-11-17 11:16:36 -0500

    First Commit

---
+++ Week 1/Example.py
@@ -0,0 +1,15 @@
+
+class PartyAnimal:
+    x = 0
+
+    def party(self):
+        self.x += 1
+        print("Partying So Far ", self.x)
+
+an = PartyAnimal()
+
+an.party()
+an.party()
+an.party()
+
+print("Hello World")
